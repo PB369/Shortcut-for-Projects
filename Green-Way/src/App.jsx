d0399a9c02dc55915c4fbe04c39cacc3c86dd3d5
PB369 <pedrohbarros369@gmail.com>
2023-12-15 21:28:05 -0300

    Deploy Version

---
+++ Green-Way/src/App.jsx
@@ -0,0 +1,16 @@
+import BackgroundContent from "./components/BackgroundContent"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <BackgroundContent/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ Green-Way/src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import './css/Header.scss'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Green Way</h1>
+    </header>
+  )
+}
+
+export default Header
